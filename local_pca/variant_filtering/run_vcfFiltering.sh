fa01b4049bac4f99b1fa35055644d93f6af0e09b
Diana Tataru <112664376+dianatataru@users.noreply.github.com>
2026-09-09 09:09:37 -0700

    Update and rename run_vcfFiltering.sh to local_pca/variant_filtering/run_vcfFiltering.sh

--- local_pca/variant_filtering/run_vcfFiltering.sh
+++ local_pca/variant_filtering/run_vcfFiltering.sh
@@ -16,25 +16,25 @@ module load bcftools
 cd /uufs/chpc.utah.edu/common/home/gompert-group3/projects/timema_SVmethods/GBS/bcftools_vcf/
 
 #plot stats on raw vcf
-#bcftools query \
-#       -f '%CHROM\t%POS\t%QUAL\t%INFO/MQBZ\t%INFO/SCBZ\t%INFO/RPBZ\t%INFO/DP\n' \
-#      FHA_all_oneref.vcf > FHA_all_oneref_site_metrics.txt
-#Rscript plot_histograms.R
+bcftools query \
+       -f '%CHROM\t%POS\t%QUAL\t%INFO/MQBZ\t%INFO/SCBZ\t%INFO/RPBZ\t%INFO/DP\n' \
+      FHA_all_oneref.vcf > FHA_all_oneref_site_metrics.txt
+Rscript plot_histograms.R
 
 #filtered the SNP set for coverage, missing data, and various tests of bias 
-#perl vcfFilter.pl REF_all_oneref.vcf
+perl vcfFilter.pl REF_all_oneref.vcf
 
 #extracted the read depth per SNP and individual from the filtered vcf files
-#bcftools query -f '[%DP\t]\n' filtered2x_REF_all_oneref.vcf | sed 's/\t$//' > depth_oneref.txt
+bcftools query -f '[%DP\t]\n' filtered2x_REF_all_oneref.vcf | sed 's/\t$//' > depth_oneref.txt
 
 #compute depth per individual and SNP to identify SNPs and individuals to drop
-#Rscript CovFilt.R
+Rscript CovFilt.R
 
 #Drop those individuals
-#perl filterSomeMore.pl --keep KeepSNPs.txt --keepinds KeepInds.txt --out morefilter_2x_REF_all_oneref.vcf filtered2x_REF_all_oneref.vcf
+perl filterSomeMore.pl --keep KeepSNPs.txt --keepinds KeepInds.txt --out morefilter_2x_REF_all_oneref.vcf filtered2x_REF_all_oneref.vcf
 
 #convert to genotype likelhood file
-#perl vcf2gl.pl 0.0 morefilter_2x_REF_all_oneref.vcf
+perl vcf2gl.pl 0.0 morefilter_2x_REF_all_oneref.vcf
 
 #obtain maximum likelihood of AF using expectation-maximization algorithm written by Zach, estpEM
 #before running, change first line to have #ind #loci instead of 0 0
